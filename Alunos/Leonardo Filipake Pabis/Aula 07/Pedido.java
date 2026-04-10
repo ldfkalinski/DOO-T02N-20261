04cbf2f54177d4c1c406845a11955809fc6bf6c5
leofpabis <leofilipake@gmail.com>
2026-04-10 11:29:47 -0300

    feita um pouco dos pedidos

--- Alunos/Leonardo Filipake Pabis/Aula 07/Pedido.java
+++ Alunos/Leonardo Filipake Pabis/Aula 07/Pedido.java
@@ -1,3 +1,26 @@
+
+import java.util.ArrayList;
+
 public class Pedido {
-    
+    private int id;
+    private Date dataCriacao;
+    private Date dataPagamento;
+    private Date dataVencimentoReserva;
+    private Clientes cliente;
+    private Vendedor vendedor;
+    private Lojas loja;
+    public ArrayList<Item> itens = new ArrayList<>();
+
+
+    public double calcularValorTotal() {
+        double soma = 0;
+        for (int i = 0; i < itens.size(); i++) {
+            soma += itens.get(i).getValor();
+        }
+        return soma;
+    }
+
+    public void gerarDescricaoVenda() {
+        System.out.println("Criação: " + dataCriacao + " | Total: " + calcularValorTotal());
+    }
 }
